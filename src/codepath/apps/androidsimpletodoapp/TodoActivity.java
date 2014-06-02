c496de06a34cf508792af118549b00959701e707
Julien Lecomte <julien.lecomte@gmail.com>
2014-06-02 11:41:26 -0700

    Serialize items after editing

--- src/codepath/apps/androidsimpletodoapp/TodoActivity.java
+++ src/codepath/apps/androidsimpletodoapp/TodoActivity.java
@@ -123,6 +123,7 @@ public class TodoActivity extends Activity {
 			int position = data.getExtras().getInt("pos");
 			items.set(position, text);
 			itemsAdapter.notifyDataSetChanged();
+			serializeItems();
 		}
 	}
 }
